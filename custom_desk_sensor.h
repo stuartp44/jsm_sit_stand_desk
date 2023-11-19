268999d80557be0a8d83613d4d1ed52f4d66e392
Stuart Pearson <stuart.pearson@philips.com>
2023-11-19 14:08:32 +0100

    swaped include

--- custom_desk_sensor.h
+++ custom_desk_sensor.h
@@ -1,21 +1,24 @@
-#pragma once
 #include "esphome.h"
+#pragma once
 
-class CustomDeskSensor : public Component, public UARTDevice {
+class CustomDeskSensor : public Component, public UARTDevice
+{
 public:
-  CustomDeskSensor(UARTComponent *parent) : UARTDevice(parent) {}
+    CustomDeskSensor(UARTComponent *parent) : UARTDevice(parent) {}
 
-  float get_desk_height() {
-    // Implement your decoding logic here
-    // Example: Replace this with actual decoding logic based on your sensor's data format
-    return 0.0;
-  }
+    float get_desk_height()
+    {
+        // Implement your decoding logic here
+        // Example: Replace this with actual decoding logic based on your sensor's data format
+        return 0.0;
+    }
 
 protected:
-  void loop() override {
-    // Add any custom loop code if needed
-  }
+    void loop() override
+    {
+        // Add any custom loop code if needed
+    }
 
 private:
-  // Add any private member variables or functions here
+    // Add any private member variables or functions here
 };
